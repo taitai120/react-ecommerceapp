81e49ba7a3581b295d1ea7c051b51031340cc88e
tai-phan <phantuantai7293@gmail.com>
2022-12-27 17:54:52 +0700

    first day 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,36 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <>
-            <footer className="py-3"></footer>
-            <footer className="py-3"></footer>
             <footer className="py-3">
                 <div className="container-xxl">
                     <div className="row">
+                        <div className="col-5">
+                            <div className="footer-top-data d-flex gap-30 align-items-center">
+                                <img
+                                    src="images/newsletter.png"
+                                    alt="newsletter"
+                                />
+                                <h2 className="mb-0 text-white">
+                                    Sign Up for Newsletter
+                                </h2>
+                            </div>
+                        </div>
+                        <div className="col-7">
+                            <input
+                                type="text"
+                                className="form-control py-2"
+                                placeholder="Search Product Here..."
+                            />
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className="py-3"></footer>
+            <footer className="py-4">
+                <div className="container-xxl">
+                    <div className="row d-flex justify-content-between">
                         <div className="col-12">
-                            <p className="text-center">
+                            <p className="text-center mb-0 text-white">
                                 &copy; {new Date().getFullYear()}; Powered by
                                 Developer's Corner
                             </p>
