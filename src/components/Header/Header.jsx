9e1442e301ff88ffca0ba94a668654cf03c263dc
tai-phan <phantuantai7293@gmail.com>
2023-01-11 13:55:50 +0700

    add url to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,7 +38,9 @@ const Header = () => {
                     <div className="row align-items-center">
                         <div className="col-2">
                             <h2>
-                                <Link className="text-white">TaiTai Store</Link>
+                                <Link to="/" className="text-white">
+                                    TaiTai Store
+                                </Link>
                             </h2>
                         </div>
                         <div className="col-5">
